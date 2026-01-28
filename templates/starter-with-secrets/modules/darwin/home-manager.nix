60a533a0bcba17cb57c0f1207ed2872e371a8816
mattangi <mattangi@gmail.com>
2026-01-28 11:30:23 +0900

    Remove Emacs launcher from darwin home-manager template

--- templates/starter-with-secrets/modules/darwin/home-manager.nix
+++ templates/starter-with-secrets/modules/darwin/home-manager.nix
@@ -3,10 +3,6 @@
 let
   user = "%USER%";
   # Define the content of your file as a derivation
-  myEmacsLauncher = pkgs.writeScript "emacs-launcher.command" ''
-    #!/bin/sh
-    emacsclient -c -n &
-  '';
   sharedFiles = import ../shared/files.nix { inherit config pkgs; };
   additionalFiles = import ./files.nix { inherit user config pkgs; };
 in
@@ -54,7 +50,6 @@ in
         file = lib.mkMerge [
           sharedFiles
           additionalFiles
-          { "emacs-launcher.command".source = myEmacsLauncher; }
         ];
 
         stateVersion = "23.11";
@@ -81,10 +76,6 @@ in
         { path = "/System/Applications/Photos.app/"; }
         { path = "/System/Applications/Photo Booth.app/"; }
         { path = "/System/Applications/System Settings.app/"; }
-        {
-          path = toString myEmacsLauncher;
-          section = "others";
-        }
         {
           path = "${config.users.users.${user}.home}/Downloads";
           section = "others";
