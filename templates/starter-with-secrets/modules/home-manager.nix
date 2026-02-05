7a277cb7433819004c09a6a2333029e6985c7f37
mattangi <mattangi@gmail.com>
2026-02-05 16:30:07 +0900

    modified dock entries.

--- templates/starter-with-secrets/modules/home-manager.nix
+++ templates/starter-with-secrets/modules/home-manager.nix
@@ -424,18 +424,22 @@ in
       username = user;
       entries = [
         { path = "/Applications/Safari.app/"; }
-        { path = "/System/Applications/Messages.app/"; }
-        { path = "/System/Applications/Notes.app/"; }
-        { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
-        { path = "/System/Applications/Music.app/"; }
-        { path = "/System/Applications/Photos.app/"; }
-        { path = "/System/Applications/Photo Booth.app/"; }
         { path = "/System/Applications/System Settings.app/"; }
         {
-          path = "${config.users.users.${user}.home}/Downloads";
-          section = "others";
-          options = "--sort name --view grid --display stack";
-        }
+            path = "/Applications";
+            section = "others";
+            options = "--sort name --view grid --display folder";
+          }
+          {
+            path = "/Users/${user}/Downloads/";
+            section = "others";
+            options = "--sort name --view grid --display folder";
+          }
+          {
+            path = "/Users/${user}/Documents";
+            section = "others";
+            options = "--sort name --view grid --display folder";
+          }
       ];
     };
   };
